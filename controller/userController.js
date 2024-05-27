66cb9b5f3e1a7579580c5399cd5538f64ddecc74
vedivero <vedivero@github.com>
2024-05-27 22:00:48 +0900

    commit

--- controller/userController.js
+++ controller/userController.js
@@ -59,7 +59,7 @@ userController.loginWithEmail = async (req, res) => {
         }
         throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
     } catch (error) {
-        res.status(400).json({ status: "login fail" }, error)
+        res.status(400).json({ status: "login fail", message: error.message });
     }
 }
 
